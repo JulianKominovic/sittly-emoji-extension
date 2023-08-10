0be0aa030d8377ad6eb8f6e8fb4a8dcfbf9ad369
Julian Kominovic <juliankominovic@gmail.com>
2023-08-10 00:38:52 -0300

    test: remove component

--- index.tsx
+++ index.tsx
@@ -15,43 +15,42 @@ const pages: ExtensionPages = [
     name: "Emojis",
     route: "/emojis",
     component: () => {
-      const setContextMenuOptions = useServices(
-        (state) => state.setContextMenuOptions
-      );
-      return (
-        <SittlyCommand.Root>
-          <SittlyCommand.Grid
-            id="emojis-page-list"
-            columns={4}
-            items={emojis.map((emoji) => {
-              return {
-                onClick() {
-                  pasteToCurrentWindow(emoji.emoji);
-                },
-                onHighlight() {
-                  setContextMenuOptions([
-                    {
-                      title: "Copy",
-                      onClick() {
-                        copyToClipboard(emoji.emoji);
-                      },
-                      description: `Copy ${emoji.emoji} to the clipboard`,
-                      icon: <BsClipboard />,
-                    },
-                  ]);
-                },
-                filteringText: emoji.description,
-                customChildren: (
-                  <div className="flex items-center justify-center text-6xl">
-                    {emoji.emoji}
-                  </div>
-                ),
-                className: "flex items-center justify-center",
-              };
-            })}
-          />
-        </SittlyCommand.Root>
-      );
+      return <>a</>;
+      // const setContextMenuOptions = useServices(
+      //   (state) => state.setContextMenuOptions
+      // );
+      // return (
+      //   <SittlyCommand.Grid
+      //     id="emojis-page-list"
+      //     columns={4}
+      //     items={emojis.map((emoji) => {
+      //       return {
+      //         onClick() {
+      //           pasteToCurrentWindow(emoji.emoji);
+      //         },
+      //         onHighlight() {
+      //           setContextMenuOptions([
+      //             {
+      //               title: "Copy",
+      //               onClick() {
+      //                 copyToClipboard(emoji.emoji);
+      //               },
+      //               description: `Copy ${emoji.emoji} to the clipboard`,
+      //               icon: <BsClipboard />,
+      //             },
+      //           ]);
+      //         },
+      //         filteringText: emoji.description,
+      //         customChildren: (
+      //           <div className="flex items-center justify-center text-6xl">
+      //             {emoji.emoji}
+      //           </div>
+      //         ),
+      //         className: "flex items-center justify-center",
+      //       };
+      //     })}
+      //   />
+      // );
     },
     description: "A collection of emojis",
     icon: <BsEmojiSmile />,
