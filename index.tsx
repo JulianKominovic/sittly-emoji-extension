e9890a405d8f436b03de5a02e4c4261f049184bd
Julian Kominovic <juliankominovic@gmail.com>
2023-08-10 00:54:35 -0300

    fix: import React

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 import { BsClipboard, BsEmojiSmile } from "react-icons/bs";
-
+import React from "react";
 import { SittlyCommand } from "sittly-devtools/dist/components/own_command";
 import * as unicodeEmoji from "unicode-emoji";
 import {
@@ -15,42 +15,41 @@ const pages: ExtensionPages = [
     name: "Emojis",
     route: "/emojis",
     component: () => {
-      return <>a</>;
-      // const setContextMenuOptions = useServices(
-      //   (state) => state.setContextMenuOptions
-      // );
-      // return (
-      //   <SittlyCommand.Grid
-      //     id="emojis-page-list"
-      //     columns={4}
-      //     items={emojis.map((emoji) => {
-      //       return {
-      //         onClick() {
-      //           pasteToCurrentWindow(emoji.emoji);
-      //         },
-      //         onHighlight() {
-      //           setContextMenuOptions([
-      //             {
-      //               title: "Copy",
-      //               onClick() {
-      //                 copyToClipboard(emoji.emoji);
-      //               },
-      //               description: `Copy ${emoji.emoji} to the clipboard`,
-      //               icon: <BsClipboard />,
-      //             },
-      //           ]);
-      //         },
-      //         filteringText: emoji.description,
-      //         customChildren: (
-      //           <div className="flex items-center justify-center text-6xl">
-      //             {emoji.emoji}
-      //           </div>
-      //         ),
-      //         className: "flex items-center justify-center",
-      //       };
-      //     })}
-      //   />
-      // );
+      const setContextMenuOptions = useServices(
+        (state) => state.setContextMenuOptions
+      );
+      return (
+        <SittlyCommand.Grid
+          id="emojis-page-list"
+          columns={4}
+          items={emojis.map((emoji) => {
+            return {
+              onClick() {
+                pasteToCurrentWindow(emoji.emoji);
+              },
+              onHighlight() {
+                setContextMenuOptions([
+                  {
+                    title: "Copy",
+                    onClick() {
+                      copyToClipboard(emoji.emoji);
+                    },
+                    description: `Copy ${emoji.emoji} to the clipboard`,
+                    icon: <BsClipboard />,
+                  },
+                ]);
+              },
+              filteringText: emoji.description,
+              customChildren: (
+                <div className="flex items-center justify-center text-6xl">
+                  {emoji.emoji}
+                </div>
+              ),
+              className: "flex items-center justify-center",
+            };
+          })}
+        />
+      );
     },
     description: "A collection of emojis",
     icon: <BsEmojiSmile />,
